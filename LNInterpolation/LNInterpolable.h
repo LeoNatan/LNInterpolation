034fe1efbce7c85484328bfa78ddd406606e0e5b
Leo Natan <lnatan@wix.com>
2018-07-09 14:27:08 +0300

    Correct enum use

--- LNInterpolation/LNInterpolable.h
+++ LNInterpolation/LNInterpolable.h
@@ -10,7 +10,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 NS_SWIFT_NAME(InterpolationBehavior)
-typedef const NSString* LNInterpolationBehavior NS_EXTENSIBLE_TYPED_ENUM;
+typedef const NSString* LNInterpolationBehavior NS_TYPED_EXTENSIBLE_ENUM;
 
 /**
  Interpolate using the default behavor of each implementation.
