af6fe851fe45dcb721ee4a17b0504c9913f11ec3
Leo Natan <leo.natan@outlook.com>
2019-04-05 20:19:13 +0300

    Upgrade project for Xcode 10.2
    Add additional demo projects for iOS and macOS.

---
+++ LNInterpolationExample/LNInterpolationExample_macOS/LNInterpolationExample_macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LNInterpolationExample_macOS
+//
+//  Created by Leo Natan (Wix) on 4/5/19.
+//  Copyright © 2019 Leo Natan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
